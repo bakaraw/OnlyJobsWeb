406432dd93e5e27357e347b864c773a9c4144f24
leyaaaan1 <paninsorolean@gmail.com>
2025-04-06 09:09:54 +0800

    fix dashboard with table of applicant

--- resources/js/Components/Dashboard/DashboardContent.jsx
+++ resources/js/Components/Dashboard/DashboardContent.jsx
@@ -10,12 +10,14 @@ import {
     Legend
 } from "chart.js";
 import JobList from "./Modal/JobList.jsx";
+import SecondaryButton from "@/Components/SecondaryButton.jsx";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export default function DashboardContent({ jobs, placements }) {
     const [showDetails, setShowDetails] = useState(false);
     const [selectedJob, setSelectedJob] = useState(null);
+    const [selectedPlacement, setSelectedPlacement] = useState(null);
 
     console.log(jobs)
     console.log(placements)
@@ -60,7 +62,7 @@ export default function DashboardContent({ jobs, placements }) {
                 </div>
 
                 <div className="dashboard-cards p-4 bg-white rounded-md shadow-md w-full">
-                    <h3 className="text-xl font-semibold mb-4">Job Placements Overview</h3>
+                    <h3 className="text-xl font-semibold mb-4">Job Applicant Overview</h3>
                     {selectedJob ? (
                         <div className="overflow-x-auto">
                             <table className="table-auto w-full border-collapse">
@@ -77,12 +79,15 @@ export default function DashboardContent({ jobs, placements }) {
                                     placements.filter(p => p.job_post_id === selectedJob.id)
 
                                         .map((placement) => (
-                                            <tr key={placement.id} className="border-t">
+                                            <tr key={placement.id} className="border-t cursor-pointer hover:bg-gray-100">
                                                 <td className="py-2">{placement.user.first_name}</td>
                                                 <td className="py-2">{placement.placement_status}</td>
                                                 <td className="py-2">{new Date(placement.created_at).toLocaleDateString()}</td>
                                                 <td className="py-2">{placement.additional_remarks}</td>
-
+                                                <td className="py-2">
+                                                    <SecondaryButton className="col-span-1 px-4 py-2 flex items-center justify-center h-full" onClick={() => handleAccept(placement)}>Accept</SecondaryButton>
+                                                    <SecondaryButton className="col-span-1 px-4 py-2 flex items-center justify-center h-full " onClick={() => handleReject(placement)}>Reject</SecondaryButton>
+                                                </td>
                                             </tr>
 
                                         ))
