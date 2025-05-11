fe5553619817c706a4276f9d8570964e066c946b
leyaaaan1 <paninsorolean@gmail.com>
2025-05-11 13:18:41 +0800

    remove duplicate slot

--- database/migrations/2025_03_28_145807_job_posts.php
+++ database/migrations/2025_03_28_145807_job_posts.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('company');
             $table->integer('slot');
             $table->integer('remaining');
-            $table->integer('slot');
 
             $table->unsignedBigInteger('status_id')->nullable();
             $table->unsignedBigInteger('degree_id')->nullable();
